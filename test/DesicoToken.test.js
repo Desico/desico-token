eebc45db5b84231042a62001b5bf7d42a4c3e644
Arturas Paleicikas <paleicikas@gmail.com>
2019-02-02 10:15:24 +0200

    logic

--- test/DesicoToken.test.js
+++ test/DesicoToken.test.js
@@ -5,7 +5,11 @@ var chaiAsPromised = require('chai-as-promised');
 chai.use(chaiAsPromised);
 chai.should();
 
-const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
+const { pauseAsync,
+  unpauseAsync,
+  addWhitelistedAsync,
+  mintFromZeroAsync,
+  redeemAsync } = require('./helpers/functions');
 const DesicoToken = artifacts.require('./DesicoToken.sol');
 
 contract('DesicoToken', function ([owner, recipient, anotherAccount, ...otherAccounts]) {
@@ -19,50 +23,6 @@ contract('DesicoToken', function ([owner, recipient, anotherAccount, ...otherAcc
   beforeEach(async function () {
     token = await DesicoToken.new();
   });
-
-  var _pauseAsync = async function (token, address, owner) {
-    (await token.paused()).should.be.equal(false);
-
-    await token.pause({ from: address }).should.be.rejectedWith(EVMRevert);
-
-    await token.pause({ from: owner }).should.be.fulfilled;
-    (await token.paused()).should.be.equal(true);
-  };
-
-  var _unpauseAsync = async function (token, address, owner) {
-    (await token.paused()).should.be.equal(true);
-
-    await token.unpause({ from: address }).should.be.rejectedWith(EVMRevert);
-
-    await token.unpause({ from: owner }).should.be.fulfilled;
-    (await token.paused()).should.be.equal(false);
-  };
-
-  var _addWhitelistedAsync = async function (token, address, owner) {
-    await token.addWhitelisted(ZERO_ADDRESS, { from: owner }).should.be.rejectedWith(EVMRevert);
-    await token.addWhitelisted(address, { from: address }).should.be.rejectedWith(EVMRevert);
-
-    (await token.isWhitelisted(address)).should.be.equal(false);
-    await token.addWhitelisted(address, { from: owner }).should.be.fulfilled;
-    (await token.isWhitelisted(address)).should.be.equal(true);
-  };
-
-  var _mintFromZeroAsync = async function (token, address, amount, owner) {
-    await token.mint(ZERO_ADDRESS, amount, { from: owner }).should.be.rejectedWith(EVMRevert);
-    await token.mint(address, amount, { from: address }).should.be.rejectedWith(EVMRevert);
-    
-    (await token.balanceOf(address)).toNumber().should.be.equal(0);
-    await token.mint(address, amount, { from: owner }).should.be.fulfilled;
-    (await token.balanceOf(address)).toNumber().should.be.equal(amount);
-  };
-
-  var _redeemAsync = async function (amount, owner, recipient, anotherAccount) {
-    await token.redeem(ZERO_ADDRESS, amount, { from: anotherAccount }).should.be.rejectedWith(EVMRevert);
-    await token.redeem(recipient, amount, { from: anotherAccount }).should.be.rejectedWith(EVMRevert);
-    await token.redeem(recipient, amount, { from: recipient }).should.be.rejectedWith(EVMRevert);
-
-    await token.redeem(recipient, amount, { from: owner }).should.be.fulfilled;
-  };
 
   describe('detailed', function () {
     it('should have a name', async function () {
@@ -100,7 +60,7 @@ contract('DesicoToken', function ([owner, recipient, anotherAccount, ...otherAcc
     });
 
     it('should mint when amount is less than cap', async function () {
-      await _mintFromZeroAsync(token, recipient, amount, owner);
+      await mintFromZeroAsync(token, recipient, amount, owner);
       (await token.totalSupply()).toNumber().should.be.equal(amount);
     });
 
@@ -110,7 +70,7 @@ contract('DesicoToken', function ([owner, recipient, anotherAccount, ...otherAcc
     });
 
     it('should fail to mint after cap is reached', async function () {
-      await _mintFromZeroAsync(token, recipient, cap, owner);
+      await mintFromZeroAsync(token, recipient, cap, owner);
 
       await token.mint(anotherAccount, 1, { from: owner }).should.be.rejectedWith(EVMRevert);
       (await token.balanceOf(recipient)).toNumber().should.be.equal(cap);
@@ -119,12 +79,12 @@ contract('DesicoToken', function ([owner, recipient, anotherAccount, ...otherAcc
     it('should mint when paused', async function () {
       (await token.paused()).should.be.equal(true);
 
-      await _mintFromZeroAsync(token, recipient, amount, owner);
+      await mintFromZeroAsync(token, recipient, amount, owner);
     });
 
     it('should mint when not paused', async function () {
-      await _unpauseAsync(token, recipient, owner);
-      await _mintFromZeroAsync(token, recipient, amount, owner);
+      await unpauseAsync(token, recipient, owner);
+      await mintFromZeroAsync(token, recipient, amount, owner);
     });
   });
 
@@ -138,25 +98,25 @@ contract('DesicoToken', function ([owner, recipient, anotherAccount, ...otherAcc
 
       (await token.totalSupply()).toNumber().should.be.equal(0);
 
-      await _mintFromZeroAsync(token, recipient, amount, owner);
+      await mintFromZeroAsync(token, recipient, amount, owner);
       (await token.balanceOf(recipient)).toNumber().should.be.equal(amount);
       (await token.totalSupply()).toNumber().should.be.equal(amount);
 
-      await _redeemAsync(amountToBurn, owner, recipient, anotherAccount);
+      await redeemAsync(token, amountToBurn, owner, recipient, anotherAccount);
       (await token.balanceOf(recipient)).toNumber().should.be.equal(amountAfterBurn);
       (await token.totalSupply()).toNumber().should.be.equal(amountAfterBurn);
     });
 
     it('should burn when not paused', async function () {
-      await _unpauseAsync(token, recipient, owner);
+      await unpauseAsync(token, recipient, owner);
 
       (await token.totalSupply()).toNumber().should.be.equal(0);
 
-      await _mintFromZeroAsync(token, recipient, amount, owner);
+      await mintFromZeroAsync(token, recipient, amount, owner);
       (await token.balanceOf(recipient)).toNumber().should.be.equal(amount);
       (await token.totalSupply()).toNumber().should.be.equal(amount);
       
-      await _redeemAsync(amountToBurn, owner, recipient, anotherAccount);
+      await redeemAsync(token, amountToBurn, owner, recipient, anotherAccount);
       (await token.balanceOf(recipient)).toNumber().should.be.equal(amountAfterBurn);
       (await token.totalSupply()).toNumber().should.be.equal(amountAfterBurn);
     });
@@ -171,27 +131,27 @@ contract('DesicoToken', function ([owner, recipient, anotherAccount, ...otherAcc
     });
 
     it('should be whitelisted', async function () {
-      await _addWhitelistedAsync(token, recipient, owner);
+      await addWhitelistedAsync(token, recipient, owner);
     });
 
     it('should not be able to transfer (not whitelisted)', async function () {
       (await token.isWhitelisted(recipient)).should.be.equal(false);
 
-      await _mintFromZeroAsync(token, recipient, amount, owner);
+      await mintFromZeroAsync(token, recipient, amount, owner);
 
       token.transfer(anotherAccount, amountToSend, { from: recipient }).should.be.rejectedWith(EVMRevert);
       (await token.balanceOf(recipient)).toNumber().should.be.equal(amount);
     });
 
     it('should be able to transfer (whitelisted)', async function () {
-      await _unpauseAsync(token, recipient, owner);
-      await _addWhitelistedAsync(token, recipient, owner);
-      await _mintFromZeroAsync(token, recipient, amount, owner);
+      await unpauseAsync(token, recipient, owner);
+      await addWhitelistedAsync(token, recipient, owner);
+      await mintFromZeroAsync(token, recipient, amount, owner);
 
       (await token.balanceOf(anotherAccount)).toNumber().should.be.equal(0);
       await token.transfer(anotherAccount, amountToSend, { from: recipient }).should.be.rejectedWith(EVMRevert);
       
-      await _addWhitelistedAsync(token, anotherAccount, owner);
+      await addWhitelistedAsync(token, anotherAccount, owner);
 
       await token.transfer(anotherAccount, amountToSend, { from: recipient }).should.be.fulfilled;
       (await token.balanceOf(anotherAccount)).toNumber().should.be.equal(amountToSend);
@@ -205,8 +165,8 @@ contract('DesicoToken', function ([owner, recipient, anotherAccount, ...otherAcc
     });
 
     it('should be able to pause/unpause', async function () {
-      await _unpauseAsync(token, recipient, owner);
-      await _pauseAsync(token, recipient, owner);
+      await unpauseAsync(token, recipient, owner);
+      await pauseAsync(token, recipient, owner);
     });
 
     it('should be able to transfer when unpaused', async function () {
@@ -215,14 +175,14 @@ contract('DesicoToken', function ([owner, recipient, anotherAccount, ...otherAcc
 
       (await token.paused()).should.be.equal(true);
 
-      await _mintFromZeroAsync(token, recipient, amount, owner);
+      await mintFromZeroAsync(token, recipient, amount, owner);
 
-      await _addWhitelistedAsync(token, recipient, owner);
-      await _addWhitelistedAsync(token, anotherAccount, owner);
+      await addWhitelistedAsync(token, recipient, owner);
+      await addWhitelistedAsync(token, anotherAccount, owner);
 
       await token.transfer(anotherAccount, amount, { from: recipient }).should.be.rejectedWith(EVMRevert);
 
-      await _unpauseAsync(token, recipient, owner);
+      await unpauseAsync(token, recipient, owner);
       
       await token.transfer(anotherAccount, amountWhenSmall, { from: recipient }).should.not.be.fulfilled;
       await token.transfer(anotherAccount, amount + amount, { from: recipient }).should.be.rejectedWith(EVMRevert);
---
+++ test/helpers/functions.js
@@ -0,0 +1,56 @@
+import EVMRevert from './EVMRevert';
+
+const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
+
+async function pauseAsync (token, address, owner) {
+    (await token.paused()).should.be.equal(false);
+
+    await token.pause({ from: address }).should.be.rejectedWith(EVMRevert);
+
+    await token.pause({ from: owner }).should.be.fulfilled;
+    (await token.paused()).should.be.equal(true);
+};
+
+async function unpauseAsync (token, address, owner) {
+    (await token.paused()).should.be.equal(true);
+
+    await token.unpause({ from: address }).should.be.rejectedWith(EVMRevert);
+
+    await token.unpause({ from: owner }).should.be.fulfilled;
+    (await token.paused()).should.be.equal(false);
+};
+
+async function addWhitelistedAsync (token, address, owner) {
+    await token.addWhitelisted(ZERO_ADDRESS, { from: owner }).should.be.rejectedWith(EVMRevert);
+    await token.addWhitelisted(address, { from: address }).should.be.rejectedWith(EVMRevert);
+
+    (await token.isWhitelisted(address)).should.be.equal(false);
+    await token.addWhitelisted(address, { from: owner }).should.be.fulfilled;
+    (await token.isWhitelisted(address)).should.be.equal(true);
+};
+
+async function mintFromZeroAsync (token, address, amount, owner) {
+    await token.mint(ZERO_ADDRESS, amount, { from: owner }).should.be.rejectedWith(EVMRevert);
+    await token.mint(address, amount, { from: address }).should.be.rejectedWith(EVMRevert);
+    
+    (await token.balanceOf(address)).toNumber().should.be.equal(0);
+    await token.mint(address, amount, { from: owner }).should.be.fulfilled;
+    (await token.balanceOf(address)).toNumber().should.be.equal(amount);
+};
+
+async function redeemAsync (token, amount, owner, recipient, anotherAccount) {
+    await token.redeem(ZERO_ADDRESS, amount, { from: anotherAccount }).should.be.rejectedWith(EVMRevert);
+    await token.redeem(recipient, amount, { from: anotherAccount }).should.be.rejectedWith(EVMRevert);
+    await token.redeem(recipient, amount, { from: recipient }).should.be.rejectedWith(EVMRevert);
+
+    await token.redeem(recipient, amount, { from: owner }).should.be.fulfilled;
+};
+
+module.exports = {
+    ZERO_ADDRESS,
+    pauseAsync,
+    unpauseAsync,
+    addWhitelistedAsync,
+    mintFromZeroAsync,
+    redeemAsync
+};
