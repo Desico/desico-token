58158617897f2475162c991528b0746fd5fa3beb
Arturas Paleicikas <paleicikas@gmail.com>
2019-02-02 16:50:10 +0200

    merge

--- contracts/DesicoToken.sol
+++ contracts/DesicoToken.sol
@@ -6,10 +6,10 @@ import "openzeppelin-solidity/contracts/token/ERC20/ERC20Pausable.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Burnable.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Capped.sol";
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
-import "./ERC20Whitelisted.sol";
+import "openzeppelin-solidity/contracts/access/roles/WhitelistedRole.sol";
 
 
-contract DesicoToken is ERC20, ERC20Detailed, ERC20Capped, ERC20Pausable, ERC20Burnable, ERC20Whitelisted, Ownable {
+contract DesicoToken is ERC20, ERC20Detailed, ERC20Capped, ERC20Pausable, ERC20Burnable, WhitelistedRole, Ownable {
   uint private constant INITIAL_SUPPLY = 1023018;
 
   constructor() public
@@ -17,7 +17,6 @@ contract DesicoToken is ERC20, ERC20Detailed, ERC20Capped, ERC20Pausable, ERC20B
     ERC20Capped(INITIAL_SUPPLY)
     ERC20Pausable()
     ERC20Burnable()
-    ERC20Whitelisted()
     ERC20()
   {
     pause();
@@ -27,5 +26,36 @@ contract DesicoToken is ERC20, ERC20Detailed, ERC20Capped, ERC20Pausable, ERC20B
     require(account != address(0), "invalid address");
 
     _burn(account, value);
+  }
+
+  function transfer(address to, uint256 value) public onlyWhitelisted returns (bool) {
+    require(isWhitelisted(to), "address not whitelisted");
+
+    return super.transfer(to, value);
+  }
+
+  function transferFrom(address from, address to, uint256 value) public onlyWhitelisted returns (bool) {
+    require(isWhitelisted(from), "address not whitelisted");
+    require(isWhitelisted(to), "address not whitelisted");
+
+    return super.transferFrom(from, to, value);
+  }
+
+  function approve(address spender, uint256 value) public onlyWhitelisted returns (bool) {
+    require(isWhitelisted(spender), "address not whitelisted");
+
+    return super.approve(spender, value);
+  }
+
+  function increaseAllowance(address spender, uint addedValue) public onlyWhitelisted returns (bool success) {
+    require(isWhitelisted(spender), "address not whitelisted");
+
+    return super.increaseAllowance(spender, addedValue);
+  }
+
+  function decreaseAllowance(address spender, uint subtractedValue) public onlyWhitelisted returns (bool success) {
+    require(isWhitelisted(spender), "address not whitelisted");
+
+    return super.decreaseAllowance(spender, subtractedValue);
   }
 }
